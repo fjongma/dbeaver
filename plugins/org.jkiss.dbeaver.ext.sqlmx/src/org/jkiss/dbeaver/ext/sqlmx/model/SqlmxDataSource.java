0bf35f460de217a24bcc2e22c14d4f9b936f8bb0
Frans Jongma (Eclipse) <frans.jongma@freedom.nl>
2025-05-25 12:57:31 +0200

    dbeaver/dbeaver#37995 SQL/MX specific additions
    Add plugins/org.jkiss/dbeaver.ext.sqlmx files
    Add sqlmx to features.xml and pom.xml
    Add icons to dbeaver.ext.generic/icons
    Add sqlmx driver details to dbeaver.ext.geneic/plugin.xml

---
+++ plugins/org.jkiss.dbeaver.ext.sqlmx/src/org/jkiss/dbeaver/ext/sqlmx/model/SqlmxDataSource.java
@@ -0,0 +1,51 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.sqlmx.model;
+
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
+import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetaModel;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
+import org.jkiss.dbeaver.model.exec.DBCException;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCExecutionContext;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+
+import java.sql.Connection;
+import org.jkiss.dbeaver.Log;
+
+/**
+ * Sqlmx datasource
+ */
+public class SqlmxDataSource extends GenericDataSource {
+    private static final Log log = Log.getLog(SqlmxDataSource.class);
+    public SqlmxDataSource(DBRProgressMonitor monitor, DBPDataSourceContainer container, GenericMetaModel metaModel)
+        throws DBException {
+        super(monitor, container, metaModel, new SqlmxSQLDialect());
+//FJ        log.debug ("****** SqlmxDataSource called ");
+    }
+
+    @Override
+    protected boolean isPopulateClientAppName() {
+    	// SQL/MX drivr does not support this type of settingthe application name
+        return false;
+    }
+    
+   
+}
---
+++ plugins/org.jkiss.dbeaver.ext.sqlmx/src/org/jkiss/dbeaver/ext/sqlmx/model/SqlmxMetaModel.java
@@ -0,0 +1,73 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.sqlmx.model;
+
+
+//FJ [HPE sqlmx]: kept this list of imports replacing informix with sqlmx. 
+import org.jkiss.dbeaver.model.DBPScriptObject;
+// will see later which one(s) are really needed.
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.DBDatabaseException;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.ext.generic.model.*;
+import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetaModel;
+
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCStatement;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.rdb.DBSForeignKeyModifyRule;
+import org.jkiss.utils.CommonUtils;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * SqlmxMetaModel
+ */
+public class SqlmxMetaModel extends GenericMetaModel
+{
+    private static final Log log = Log.getLog(SqlmxMetaModel.class);
+
+    public SqlmxMetaModel() {
+    	super();
+        //log.debug("***************Sqlmx phase2: SqlmxMetaModel called");
+    }
+
+    @Override
+    public SqlmxDataSource createDataSourceImpl(DBRProgressMonitor monitor, DBPDataSourceContainer container) throws DBException {
+        return new SqlmxDataSource(monitor, container, new SqlmxMetaModel());
+    }
+
+  
+    @Override
+    public String getTableDDL(@NotNull DBRProgressMonitor monitor, @NotNull GenericTableBase sourceObject, @NotNull Map<String, Object> options) throws DBException {
+    	String tableDDL = super.getTableDDL(monitor, sourceObject, options);
+    	return tableDDL;
+
+    }
+
+
+}
